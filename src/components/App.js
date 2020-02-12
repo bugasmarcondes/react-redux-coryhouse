0d424bc693748669d8cd3c3dc8f37741f5abf065
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-02-12 06:38:33 -0300

    - Aplicando Switch nas rotas - Renderizando PageNotFound se não encontrar nenhuma rota

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,18 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
 
 function App() {
     return (
         <div className="container-fluid">
             <Header />
+            {/* Utilizamos Switch para que somente uma rota seja encontrada e utilizada */}
+            {/* Se não encontrar uma rota, utiliza o componente PageNotFound */}
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/about" component={AboutPage} />
+                <Route component={PageNotFound} />
             </Switch>
         </div>
     );

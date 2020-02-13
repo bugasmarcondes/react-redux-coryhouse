aebb6d1163698d6044b929f67eac702d22b8dc78
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-02-13 06:50:16 -0300

    Aplicando a infraestrutura para o Redux 1. Criação da courseActions 2. Criação do courseReducer 3. Criação do rootReducer 4. Criação da configureStore 5. Importação da store e passagem para o App via Provider do ReactRedux

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
 
 // testando eslint
 // myGlobal = 4;
 
+const store = configureStore();
+
 render(
-    <Router>
-        <App />
-    </Router>,
+    <ReduxProvider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </ReduxProvider>,
     document.getElementById("app")
 );

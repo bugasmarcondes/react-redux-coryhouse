0292a103523534bde155744874311aac4ef7fb88
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-02-19 06:57:04 -0300

    a. courseActions.js, adicionamos a ação para carregamento dos cursos b. actionTypes.js, registramos uma constante para a nova ação c. courseReducer.js, tratamos a ação criada anteriormente, e retornamos o novo state via reducer

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,25 @@
 import * as types from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
 
 export function createCourse(course) {
     return { type: types.CREATE_COURSE, course };
+}
+
+export function loadCourseSuccess(courses) {
+    return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+    // our first thunk
+    // redux thunk injecs dispatch automatically
+    return function(dispatch) {
+        return courseApi
+            .getCourses()
+            .then(courses => {
+                dispatch(loadCourseSuccess(courses));
+            })
+            .catch(error => {
+                throw error;
+            });
+    };
 }

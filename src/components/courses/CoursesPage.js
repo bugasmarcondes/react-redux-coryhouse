6399356caec7bb8969c2a5d8d1e2a95dc4e8acc7
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-02-19 06:58:20 -0300

    Carregamos os cursos ao exibir o componente

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,52 +5,20 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 
 class CoursesPage extends Component {
-    // 1. e 2.
-    // constructor(props) {
-    //     super(props);
-
-    //     this.state = {
-    state = {
-        course: {
-            title: "",
-        },
-    };
-
-    // 2. binding in the constructor, melhor do que opção 1
-    // this.handleChange = this.handleChange.bind(this);
-    //}
-
-    // 1. e 2. handleChange(event) {
-    // 3. arrow functions inherit the binding context of their enclosing scope
-    handleChange = event => {
-        const course = { ...this.state.course, title: event.target.value };
-        this.setState({ course });
-    };
-
-    handleSubmit = event => {
-        event.preventDefault();
-        this.props.actions.createCourse(this.state.course);
-    };
+    componentDidMount() {
+        this.props.actions.loadCourses().catch(error => {
+            alert("Loading courses failed " + error);
+        });
+    }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <>
                 <h2>Courses</h2>
-                <h3>Add Course</h3>
-                {/* 1. não fazer esse tipo de bind, pois toda vez que digitamos, uma nova função é criada no change */}
-                {/* onChange={this.handleChange.bind(this)} */}
-                <input
-                    type="text"
-                    onChange={this.handleChange}
-                    value={this.state.course.title}
-                />
-
-                <input type="submit" value="Save" />
-
                 {this.props.courses.map(course => (
                     <div key={course.title}>{course.title}</div>
                 ))}
-            </form>
+            </>
         );
     }
 }

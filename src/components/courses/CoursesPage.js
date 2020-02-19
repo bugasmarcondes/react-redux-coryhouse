82a34d84fb050a0c866e278bedd4de6e29bd9629
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-02-19 07:50:49 -0300

    - Aplicando redux flow em nova funcionalidade, para exibir nome dos autores 1. actionTypes 2. authorActions 3. authorReducer 4. indexReducer (root) 5. CoursesPage (container component) 6. CourseList (presentational component)

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 
 class CoursesPage extends Component {
     componentDidMount() {
-        this.props.actions.loadCourses().catch(error => {
-            alert("Loading courses failed " + error);
-        });
+        const { courses, authors, actions } = this.props;
+
+        if (courses.length === 0) {
+            actions.loadCourses().catch(error => {
+                alert("Loading courses failed " + error);
+            });
+        }
+
+        if (authors.length === 0) {
+            actions.loadAuthors().catch(error => {
+                alert("Loading authors failed " + error);
+            });
+        }
     }
 
     render() {
@@ -24,12 +35,24 @@ class CoursesPage extends Component {
 
 CoursesPage.propTypes = {
     courses: PropTypes.array.isRequired,
+    authors: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state) {
     return {
-        courses: state.courses,
+        courses:
+            state.authors.length === 0
+                ? []
+                : state.courses.map(course => {
+                      return {
+                          ...course,
+                          authorName: state.authors.find(
+                              a => a.id === course.authorId
+                          ).name,
+                      };
+                  }),
+        authors: state.authors,
     };
 }
 
@@ -41,7 +64,16 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        actions: bindActionCreators(courseActions, dispatch),
+        actions: {
+            loadCourses: bindActionCreators(
+                courseActions.loadCourses,
+                dispatch
+            ),
+            loadAuthors: bindActionCreators(
+                authorActions.loadAuthors,
+                dispatch
+            ),
+        },
     };
 }
 
